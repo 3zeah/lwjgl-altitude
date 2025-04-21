3dc15d12e9e2efbc0c9fda4b148876bb623b8794
dvd <dvd>
2025-04-21 23:31:04 +0200

    copy-paste controller support from lwjgl2
    this is a wrapper around junit anyway, and is completely independent of all other lwjgl systems

--- module/lwjgl2-layer/util/src/org/lwjgl/LWJGLException.java
+++ module/lwjgl2-layer/util/src/org/lwjgl/LWJGLException.java
@@ -36,4 +36,8 @@ public class LWJGLException extends Exception {
     public LWJGLException(String message) {
         super(message);
     }
+
+    public LWJGLException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
