ade1d7c3ecf201d70a7ebd6cd8ac561d685c9160
dvd <dvd>
2025-04-21 23:41:13 +0200

    remove dead controller api:s for posterity

--- module/lwjgl2-layer/input/src/org/lwjgl/input/Controller.java
+++ module/lwjgl2-layer/input/src/org/lwjgl/input/Controller.java
@@ -47,27 +47,11 @@ public interface Controller {
 	String getName();
 
 	/**
-	 * Get the index of this controller in the collection
-	 *
-	 * @return The index of this controller in the collection
-	 */
-	int getIndex();
-
-	/**
 	 * Retrieve the number of buttons available on this controller
 	 *
 	 * @return The number of butotns available on this controller
 	 */
 	int getButtonCount();
-
-	/**
-	 * Get the name of the specified button. Be warned, often this is
-	 * as exciting as "Button X"
-	 *
-	 * @param index The index of the button whose name should be retrieved
-	 * @return The name of the button requested
-	 */
-	String getButtonName(int index);
 
 	/**
 	 * Check if a button is currently pressed
@@ -98,22 +82,6 @@ public interface Controller {
 	float getPovY();
 
 	/**
-	 * Get the dead zone for a specified axis
-	 *
-	 * @param index The index of the axis for which to retrieve the dead zone
-	 * @return The dead zone for the specified axis
-	 */
-	float getDeadZone(int index);
-
-	/**
-	 * Set the dead zone for the specified axis
-	 *
-	 * @param index The index of hte axis for which to set the dead zone
-	 * @param zone The dead zone to use for the specified axis
-	 */
-	void setDeadZone(int index,float zone);
-
-	/**
 	 * Retrieve the number of axes available on this controller.
 	 *
 	 * @return The number of axes available on this controller.
@@ -138,153 +106,4 @@ public interface Controller {
 	 * @return The value from the specified axis.
 	 */
 	float getAxisValue(int index);
-
-	/**
-	 * Get the value from the X axis if there is one. If no X axis is
-	 * defined a zero value will be returned.
-	 *
-	 * @return The value from the X axis
-	 */
-	float getXAxisValue();
-
-	/**
-	 * Get the dead zone for the X axis.
-	 *
-	 * @return The dead zone for the X axis
-	 */
-	float getXAxisDeadZone();
-
-	/**
-	 * Set the dead zone for the X axis
-	 *
-	 * @param zone The dead zone to use for the X axis
-	 */
-	void setXAxisDeadZone(float zone);
-
-	/**
-	 * Get the value from the Y axis if there is one. If no Y axis is
-	 * defined a zero value will be returned.
-	 *
-	 * @return The value from the Y axis
-	 */
-	float getYAxisValue();
-
-	/**
-	 * Get the dead zone for the Y axis.
-	 *
-	 * @return The dead zone for the Y axis
-	 */
-	float getYAxisDeadZone();
-
-	/**
-	 * Set the dead zone for the Y axis
-	 *
-	 * @param zone The dead zone to use for the Y axis
-	 */
-	void setYAxisDeadZone(float zone);
-
-	/**
-	 * Get the value from the Z axis if there is one. If no Z axis is
-	 * defined a zero value will be returned.
-	 *
-	 * @return The value from the Z axis
-	 */
-	float getZAxisValue();
-
-	/**
-	 * Get the dead zone for the Z axis.
-	 *
-	 * @return The dead zone for the Z axis
-	 */
-	float getZAxisDeadZone();
-
-	/**
-	 * Set the dead zone for the Z axis
-	 *
-	 * @param zone The dead zone to use for the Z axis
-	 */
-	void setZAxisDeadZone(float zone);
-
-	/**
-	 * Get the value from the RX axis if there is one. If no RX axis is
-	 * defined a zero value will be returned.
-	 *
-	 * @return The value from the RX axis
-	 */
-	float getRXAxisValue();
-
-	/**
-	 * Get the dead zone for the RX axis.
-	 *
-	 * @return The dead zone for the RX axis
-	 */
-	float getRXAxisDeadZone();
-
-	/**
-	 * Set the dead zone for the RX axis
-	 *
-	 * @param zone The dead zone to use for the RX axis
-	 */
-	void setRXAxisDeadZone(float zone);
-
-	/**
-	 * Get the value from the RY axis if there is one. If no RY axis is
-	 * defined a zero value will be returned.
-	 *
-	 * @return The value from the RY axis
-	 */
-	float getRYAxisValue();
-
-	/**
-	 * Get the dead zone for the RY axis.
-	 *
-	 * @return The dead zone for the RY axis
-	 */
-	float getRYAxisDeadZone();
-
-	/**
-	 * Set the dead zone for the RY axis
-	 *
-	 * @param zone The dead zone to use for the RY axis
-	 */
-	void setRYAxisDeadZone(float zone);
-
-	/**
-	 * Get the value from the RZ axis if there is one. If no RZ axis is
-	 * defined a zero value will be returned.
-	 *
-	 * @return The value from the RZ axis
-	 */
-	float getRZAxisValue();
-
-	/**
-	 * Get the dead zone for the RZ axis.
-	 *
-	 * @return The dead zone for the RZ axis
-	 */
-	float getRZAxisDeadZone();
-
-	/**
-	 * Set the dead zone for the RZ axis
-	 *
-	 * @param zone The dead zone to use for the RZ axis
-	 */
-	void setRZAxisDeadZone(float zone);
-	
-	
-	/** Returns the number of rumblers this controller supports */
-	int getRumblerCount();
-	
-	/** Returns the name of the specified rumbler
-	 * 
-	 * @param index The rumbler index
-	 */
-	String getRumblerName(int index);
-	
-	/** Sets the vibration strength of the specified rumbler
-	 * 
-	 * @param index The index of the rumbler
-	 * @param strength The strength to vibrate at
-	 */
-	void setRumblerStrength(int index, float strength);
 }
--- module/lwjgl2-layer/input/src/org/lwjgl/input/Controllers.java
+++ module/lwjgl2-layer/input/src/org/lwjgl/input/Controllers.java
@@ -47,11 +47,6 @@ public class Controllers {
 	/** The number of controllers */
 	private static int controllerCount;
 
-	/** The current list of events */
-	private static ArrayList<ControllerEvent> events = new ArrayList<ControllerEvent>();
-	/** The current event */
-	private static ControllerEvent event;
-
 	/** Whether controllers were created */
 	private static boolean created;
 
@@ -125,36 +120,9 @@ public class Controllers {
 	}
 
 	/**
-	 * Poll the controllers available. This will both update their state
-	 * and generate events that must be cleared.
-	 */
-	public static void poll() {
-		for (int i=0;i<controllers.size();i++) {
-			getController(i).poll();
-		}
-	}
-
-	/**
 	 * Clear any events stored for the controllers in this set
 	 */
 	public static void clearEvents() {
-		events.clear();
-	}
-
-	/**
-	 * Move to the next event that has been stored.
-	 *
-	 * @return True if there is still an event to process
-	 */
-	public static boolean next() {
-		if (events.size() == 0) {
-			event = null;
-			return false;
-		}
-
-		event = events.remove(0);
-
-		return event != null;
 	}
 
 	/**
@@ -184,124 +152,5 @@ public class Controllers {
 //		events.clear();
 //		controllers.clear();
 //		controllerCount = 0;
-	}
-
-	/**
-	 * Get the source of the current event
-	 *
-	 * @return The source of the current event
-	 */
-	public static Controller getEventSource() {
-		return event.getSource();
-	}
-
-	/**
-	 * Get the index of the control that caused the current event
-	 *
-	 * @return The index of the control that cause the current event
-	 */
-	public static int getEventControlIndex() {
-		return event.getControlIndex();
-	}
-
-	/**
-	 * Check if the current event was caused by a button
-	 *
-	 * @return True if the current event was caused by a button
-	 */
-	public static boolean isEventButton() {
-		return event.isButton();
-	}
-
-	/**
-	 * Check if the current event was caused by a axis
-	 *
-	 * @return True if the current event was caused by a axis
-	 */
-	public static boolean isEventAxis() {
-		return event.isAxis();
-	}
-
-	/**
-	 * Check if the current event was caused by movement on the x-axis
-	 *
-	 * @return True if the current event was cause by movement on the x-axis
-	 */
-	public static boolean isEventXAxis() {
-		return event.isXAxis();
-	}
-
-	/**
-	 * Check if the current event was caused by movement on the y-axis
-	 *
-	 * @return True if the current event was caused by movement on the y-axis
-	 */
-	public static boolean isEventYAxis() {
-		return event.isYAxis();
-	}
-
-	/**
-	 * Check if the current event was cause by the POV x-axis
-	 *
-	 * @return True if the current event was caused by the POV x-axis
-	 */
-	public static boolean isEventPovX() {
-		return event.isPovX();
-	}
-
-	/**
-	 * Check if the current event was cause by the POV x-axis
-	 *
-	 * @return True if the current event was caused by the POV x-axis
-	 */
-	public static boolean isEventPovY() {
-		return event.isPovY();
-	}
-
-	/**
-	 * Get the timestamp assigned to the current event
-	 *
-	 * @return The timestamp assigned to the current event
-	 */
-	public static long getEventNanoseconds() {
-		return event.getTimeStamp();
-	}
-
-	/**
-	 * Gets the state of the button that generated the current event
-	 *  
-	 * @return True if button was down, or false if released
-	 */
-	public static boolean getEventButtonState() {
-		return event.getButtonState();
-	}
-
-	/**
-	 * Get the value on an X axis of the current event 
-	 *  
-	 * @return The value on a x axis of the current event
-	 */
-	public static float getEventXAxisValue() {
-		return event.getXAxisValue();
-	}
-
-	/**
-	 * Get the value on an Y axis of the current event 
-	 *  
-	 * @return The value on a y axis of the current event
-	 */
-	public static float getEventYAxisValue() {
-		return event.getYAxisValue();
-	}
-
-	/**
-	 * Add an event to the stack of events that have been caused
-	 *
-	 * @param event The event to add to the list
-	 */
-	static void addEvent(ControllerEvent event) {
-		if (event != null) {
-			events.add(event);
-		}
 	}
 }
